9fbdab6b6fffec57cf3a64311fb0d9f5630ff9a0
Jonathan Hellwig <jonathan.hellwig@posteo.de>
2023-02-02 14:03:05 +0100

    Save transformed images

--- object_detection_data.py
+++ object_detection_data.py
@@ -1,3 +1,4 @@
+import shutil
 from typing import List, Tuple
 import torch
 import itertools
@@ -5,6 +6,8 @@ import os
 import numpy as np
 import tqdm
 from PIL import Image
+import torchvision
+import torchvision.transforms as T
 
 
 class Encoder:
@@ -78,6 +81,8 @@ class ObjectDetectionDataset(torch.utils.data.Dataset):
             self.images[idx]
         label_path = self.data_path + '/labels/' + \
             self.labels[idx]
+        # print(image_path)
+        # print(label_path)
         image = Image.open(image_path)
         label_strings = open(label_path).read().splitlines()
         bounding_boxes = []
@@ -98,6 +103,22 @@ class ObjectDetectionDataset(torch.utils.data.Dataset):
         return len(self.images)
 
 
+class TransformedObjectDetectionDataset(torch.utils.data.Dataset):
+    def __init__(self, data_path: str, encoder: Encoder) -> None:
+        self.encoder = encoder
+        self.images = torch.load(data_path + '/transformed_images.pt')
+        self.bounding_boxes = torch.load(data_path + '/bounding_boxes.pt')
+        self.object_classes = torch.load(data_path + '/object_classes.pt')
+
+    def __getitem__(self, idx):
+        encoded_bounding_boxes, target_mask, target_classes = self.encoder.apply(
+            self.object_classes[idx], self.bounding_boxes[idx])
+        return self.images[idx], encoded_bounding_boxes, target_mask, target_classes
+
+    def __len__(self):
+        return len(self.images)
+
+
 def calculate_mean_std(dataset: ObjectDetectionDataset):
     means = []
     for image, _, _ in tqdm.tqdm(dataset):
@@ -106,3 +127,72 @@ def calculate_mean_std(dataset: ObjectDetectionDataset):
     mean = torch.mean(stacked_means, dim=0)
     std = torch.std(stacked_means, dim=0)
     return mean, std
+
+
+def create_train_val_split():
+    images = os.listdir('SPLObjDetectDatasetV2/train/images/')
+    index = torch.randperm(len(images))
+    val_index = index[:int(0.2 * len(images))]
+    val_files = [images[idx][:-3] for idx in val_index]
+    for files in val_files:
+        shutil.move('SPLObjDetectDatasetV2/train/images/' +
+                    files + 'png', 'SPLObjDetectDatasetV2/val/images/' + files + 'png')
+        shutil.move('SPLObjDetectDatasetV2/train/labels/' +
+                    files + 'txt', 'SPLObjDetectDatasetV2/val/labels/' + files + 'txt')
+
+
+def save_bounding_boxes(data_path):
+    labels = sorted(os.listdir(data_path + '/labels/'))
+    bounding_boxes = []
+    object_classes = []
+    for label_path in labels:
+        label_strings = open(data_path + '/labels/' +
+                             label_path).read().splitlines()
+        bounding_boxes_image = []
+        object_classes_image = []
+        for label_string in label_strings:
+            bounding_boxes_image.append(torch.tensor(np.fromstring(
+                label_string[1:], sep=' ', dtype=np.float32)))
+            object_classes_image.append(torch.tensor(np.fromstring(
+                label_string[0], sep=' ', dtype=np.float32), dtype=torch.long))
+        bounding_boxes.append(bounding_boxes_image)
+        object_classes.append(object_classes_image)
+    torch.save(bounding_boxes, data_path + '/bounding_boxes.pt')
+    torch.save(object_classes, data_path + '/object_classes.pt')
+
+
+def save_transformed_images(data_path):
+    transforms = T.Compose([T.Grayscale(), T.PILToTensor(), T.ConvertImageDtype(
+        torch.float32), T.Resize((60, 80))])
+    default_scalings = [torch.tensor(
+        [0.25, 0.25]), torch.tensor(
+        [0.125, 0.125]), torch.tensor([0.125 / 2, 0.125 / 2]), torch.tensor([0.125 / 4, 0.125 / 4])]
+    feature_map_size = (8, 10)
+    num_classes = 4
+    encoder = Encoder(default_scalings, feature_map_size, num_classes)
+    train_data = ObjectDetectionDataset(
+        data_path, encoder, transforms=transforms)
+    images = []
+    image_means = []
+    i = 0
+    for image, _, _, _ in train_data:
+        # print(image.shape)
+        images.append(image)
+        images.append(torchvision.transforms.functional.hflip(image))
+        image_means.append(torch.mean(image, dim=(1, 2)))
+        i += 1
+        if i % 10 == 0:
+            print(f'{i}/{len(train_data)}')
+    image_means = torch.tensor(image_means)
+    image_mean = torch.mean(image_means)
+    image_std = torch.std(image_means)
+    stacked_images = (torch.stack(images) - image_mean) / image_std
+    torch.save(stacked_images, data_path + '/transformed_images.pt')
+
+
+if __name__ == "__main__":
+    save_bounding_boxes('SPLObjDetectDatasetV2/train')
+    save_bounding_boxes('SPLObjDetectDatasetV2/val')
+
+    save_transformed_images('SPLObjDetectDatasetV2/val')
+    save_transformed_images('SPLObjDetectDatasetV2/train')
