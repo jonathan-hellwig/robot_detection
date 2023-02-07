85049f8ce2f9c54e6b0d6cbd066278d1f1763a69
Jonathan Hellwig <jonathan.hellwig@posteo.de>
2023-02-07 11:23:02 +0100

    Speed up bounding box encoding

--- object_detection_data.py
+++ object_detection_data.py
@@ -1,7 +1,6 @@
 import shutil
-from typing import List, Tuple
+from typing import Tuple
 import torch
-import itertools
 import os
 import numpy as np
 import tqdm
@@ -11,58 +10,109 @@ import torchvision.transforms as T
 
 
 class Encoder:
-    def __init__(self, default_scalings: List[torch.Tensor], feature_map_size: Tuple[int, int], num_classes: int, threshold: float = 0.5) -> None:
+    def __init__(self, default_scalings: torch.Tensor, feature_map_size: Tuple[int, int], num_classes: int, threshold: float = 0.5) -> None:
+
         self.default_scalings = default_scalings
         self.feature_map_size = feature_map_size
         self.num_classes = num_classes
         self.threshold = threshold
+        self.default_boxes_tr_bl = self._default_boxes_tr_bl()
+        self.default_boxes_xy_hw = self._default_boxes_xy_hw()
 
-    # TODO: Validate encoding
-    def apply(self, object_classes, bounding_boxes):
+    def apply(self, target_boxes: torch.Tensor, target_classes: torch.Tensor):
+        # Select the default box with the highest IoU and with IoU higher than the threshold value
+        target_boxes_tl_br = torch.zeros((target_boxes.size(0), 4))
+        target_boxes_tl_br[:, 0:2] = target_boxes[:,
+                                                  0:2] - target_boxes[:, 3:4] / 2
+        target_boxes_tl_br[:,
+                           2:4] = target_boxes[:, 0:2] + target_boxes[:, 3:4] / 2
+        ious = calc_iou_tensor(target_boxes_tl_br,
+                               self.default_boxes_tr_bl)
+        _, best_dbox_idx = ious.max(dim=1)
+        masked_ious = torch.logical_or((best_dbox_idx == torch.arange(
+            0, self.default_boxes_tr_bl.size(0)).reshape(-1, 1)).T, ious > self.threshold) * ious
+        # Select the target box with the highest IoU for each default box
+        best_value, best_idx = masked_ious.max(dim=0)
+
+        mask_idx = best_value > 0
+        selected_target_boxes = target_boxes[best_idx[mask_idx]]
+        selected_default_boxes = self.default_boxes_xy_hw[mask_idx]
+
+        # Encode target boxes with relative offsets to default box
+        encoded_target_boxes = torch.zeros((mask_idx.sum(0), 4))
+        encoded_target_boxes[:, 0:2] = (selected_default_boxes[:, 0:2] -
+                                        selected_target_boxes[:, 0:2]) / selected_default_boxes[:, 2:4]
+        encoded_target_boxes[:, 2:4] = torch.log(
+            selected_target_boxes[:, 2:4] / selected_default_boxes[:, 2:4])
+
+        dbox_classes = torch.zeros(
+            self.default_boxes_xy_hw.size(0), dtype=torch.long)
+
+        dbox_classes[mask_idx] = target_classes[best_idx[mask_idx]].flatten()
+        return encoded_target_boxes, mask_idx, dbox_classes
+
+    def _default_boxes_tr_bl(self):
         NUM_BOX_PARAMETERS = 4
         feature_map_height, feature_map_width = self.feature_map_size
-        encoded_bounding_boxes = torch.zeros(
-            (feature_map_height, feature_map_width, len(self.default_scalings), NUM_BOX_PARAMETERS))
-        target_mask = torch.zeros(
-            (feature_map_height, feature_map_width), dtype=torch.bool)
-        target_classes = torch.zeros(
-            (feature_map_height, feature_map_width, len(self.default_scalings)), dtype=torch.long)
-        for target_box, ground_truth_class in zip(bounding_boxes, object_classes):
-            ground_truth_center = target_box[0:2]
-            ground_truth_size = target_box[2:4]
-            jaccard_overlaps = []
-            for i, j in itertools.product(range(feature_map_height), range(feature_map_width)):
-                for k, default_scaling in enumerate(self.default_scalings):
-                    default_box_center = torch.tensor(
-                        [i / feature_map_width, j / feature_map_height])
-                    offset_box = torch.concat(
-                        ((ground_truth_center-default_box_center) / default_scaling, torch.log(ground_truth_size / default_scaling)))
-                    # TODO: Think about the scaling
-                    jaccard_overlaps.append((jaccard_overlap(default_box_center,
-                                                             default_scaling, ground_truth_center, ground_truth_size), (i, j, k), offset_box))
-            max_overlap = max(jaccard_overlaps, key=lambda item: item[0])
-            threshold_overlap = [
-                (overlap, index, offset_box) for overlap, index, offset_box in jaccard_overlaps if overlap > self.threshold]
-            # TODO: Resolve conflicts between boxes
-            for _, (i, j, k), offset_box in [max_overlap] + threshold_overlap:
-                encoded_bounding_boxes[i, j, k, :] = offset_box
-                target_mask[i, j] = True
-                target_classes[i, j, :] = ground_truth_class + 1
-        target_classes = target_classes.flatten()
-        return encoded_bounding_boxes, target_mask, target_classes
+        default_boxes = torch.zeros(
+            (feature_map_height * feature_map_width * self.default_scalings.size(0), NUM_BOX_PARAMETERS))
+        for i in range(default_boxes.size(0) // self.default_scalings.size(0)):
+            selected_boxes_idx = range(
+                i*self.default_scalings.size(0), (i+1)*self.default_scalings.size(0))
+            top_left = torch.tensor(
+                [(i % feature_map_width) / feature_map_width, (i // feature_map_width) / feature_map_height])
+            default_boxes[selected_boxes_idx, 0:2] = top_left
+            default_boxes[selected_boxes_idx,
+                          2:4] = top_left + self.default_scalings
+        return default_boxes
+
+    def _default_boxes_xy_hw(self):
+        NUM_BOX_PARAMETERS = 4
+        feature_map_height, feature_map_width = self.feature_map_size
+        default_boxes = torch.zeros(
+            (feature_map_height * feature_map_width * self.default_scalings.size(0), NUM_BOX_PARAMETERS))
+        for i in range(default_boxes.size(0) // self.default_scalings.size(0)):
+            selected_boxes_idx = range(
+                i*self.default_scalings.size(0), (i+1)*self.default_scalings.size(0))
+            top_left = torch.tensor(
+                [(i % feature_map_width) / feature_map_width, (i // feature_map_width) / feature_map_height])
+            default_boxes[selected_boxes_idx,
+                          0:2] = top_left + self.default_scalings/2
+            default_boxes[selected_boxes_idx,
+                          2:4] = self.default_scalings
+        return default_boxes
 
 
-def jaccard_overlap(center_first: torch.Tensor, size_first: torch.Tensor, center_second: torch.Tensor, size_second: torch.Tensor) -> float:
-    upper_left_first = center_first - size_first / 2
-    lower_right_first = center_first + size_first / 2
-    upper_left_second = center_second - size_second / 2
-    lower_right_second = center_second + size_second / 2
+def calc_iou_tensor(box1, box2):
+    """ Calculation of IoU based on two boxes tensor,
+        Reference to https://github.com/kuangliu/pytorch-src
+        input:
+            box1 (N, 4)
+            box2 (M, 4)
+        output:
+            IoU (N, M)
+    """
+    N = box1.size(0)
+    M = box2.size(0)
 
-    intersection_area = max(0, min(lower_right_first[0], lower_right_second[0]) - max(upper_left_first[0], upper_left_second[0])) * max(
-        0, min(lower_right_first[1], lower_right_second[1]) - max(upper_left_first[1], upper_left_second[1]))
-    union_area = size_first[0] * size_first[1] + \
-        size_second[0] * size_second[1] - intersection_area
-    return intersection_area / union_area
+    be1 = box1.unsqueeze(1).expand(-1, M, -1)
+    be2 = box2.unsqueeze(0).expand(N, -1, -1)
+
+    # Left Top & Right Bottom
+    lt = torch.max(be1[:, :, :2], be2[:, :, :2])
+    rb = torch.min(be1[:, :, 2:], be2[:, :, 2:])
+
+    delta = rb - lt
+    delta[delta < 0] = 0
+    intersect = delta[:, :, 0]*delta[:, :, 1]
+
+    delta1 = be1[:, :, 2:] - be1[:, :, :2]
+    area1 = delta1[:, :, 0]*delta1[:, :, 1]
+    delta2 = be2[:, :, 2:] - be2[:, :, :2]
+    area2 = delta2[:, :, 0]*delta2[:, :, 1]
+
+    iou = intersect/(area1 + area2 - intersect)
+    return iou
 
 
 class ObjectDetectionDataset(torch.utils.data.Dataset):
@@ -76,28 +126,28 @@ class ObjectDetectionDataset(torch.utils.data.Dataset):
             data_path + '/labels/'))
 
     def __getitem__(self, idx):
-        # TODO: This might break if one label file is missing!
         image_path = self.data_path + '/images/' + \
             self.images[idx]
         label_path = self.data_path + '/labels/' + \
             self.labels[idx]
-        # print(image_path)
-        # print(label_path)
         image = Image.open(image_path)
         label_strings = open(label_path).read().splitlines()
-        bounding_boxes = []
-        object_classes = []
-        # TODO: Use the encoding here
+        target_bounding_boxes = []
+        target_classes = []
         for label_string in label_strings:
-            bounding_boxes.append(torch.tensor(np.fromstring(
+            target_bounding_boxes.append(np.array(np.fromstring(
                 label_string[1:], sep=' ', dtype=np.float32)))
-            object_classes.append(torch.tensor(np.fromstring(
-                label_string[0], sep=' ', dtype=np.float32), dtype=torch.long))
+            target_classes.append(np.array(np.fromstring(
+                label_string[0], sep=' ', dtype=np.float32), dtype=np.int64))
+        target_bounding_boxes = torch.tensor(np.array(target_bounding_boxes))
+        target_classes = torch.tensor(np.array(target_classes)) + 1
+
         encoded_bounding_boxes, target_mask, target_classes = self.encoder.apply(
-            object_classes, bounding_boxes)
+            target_bounding_boxes, target_classes)
+
         if self.transforms is not None:
             image = self.transforms(image)
-        return image, encoded_bounding_boxes, target_mask, target_classes
+        return image, encoded_bounding_boxes, target_classes
 
     def __len__(self):
         return len(self.images)
@@ -107,13 +157,20 @@ class TransformedObjectDetectionDataset(torch.utils.data.Dataset):
     def __init__(self, data_path: str, encoder: Encoder) -> None:
         self.encoder = encoder
         self.images = torch.load(data_path + '/transformed_images.pt')
-        self.bounding_boxes = torch.load(data_path + '/bounding_boxes.pt')
-        self.object_classes = torch.load(data_path + '/object_classes.pt')
+        bounding_boxes = torch.load(data_path + '/bounding_boxes.pt')
+        object_classes = torch.load(data_path + '/object_classes.pt')
+        self.encoded_bounding_boxes = []
+        self.encoded_object_classes = []
+        self.target_masks = []
+        for bounding_box, object_class in zip(bounding_boxes, object_classes):
+            encoded_bounding_boxes, target_mask, target_classes = self.encoder.apply(
+                bounding_box, object_class)
+            self.encoded_bounding_boxes.append(encoded_bounding_boxes)
+            self.encoded_object_classes.append(target_classes)
+            self.target_masks.append(target_mask)
 
     def __getitem__(self, idx):
-        encoded_bounding_boxes, target_mask, target_classes = self.encoder.apply(
-            self.object_classes[idx], self.bounding_boxes[idx])
-        return self.images[idx], encoded_bounding_boxes, target_mask, target_classes
+        return self.images[idx], self.encoded_bounding_boxes[idx], self.target_masks[idx], self.encoded_object_classes[idx]
 
     def __len__(self):
         return len(self.images)
@@ -178,7 +235,6 @@ def save_transformed_images(data_path):
     image_means = []
     i = 0
     for image, _, _, _ in train_data:
-        # print(image.shape)
         images.append(image)
         images.append(torchvision.transforms.functional.hflip(image))
         image_means.append(torch.mean(image, dim=(1, 2)))
